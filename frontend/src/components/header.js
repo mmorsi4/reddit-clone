9816aebb6411ee7acbdffc3ab3a5ae3268e72cfa
Mohamed Morsi <mo.morsi2004@gmail.com>
2025-10-31 17:42:48 +0200

    Made a header ocomponent and fixed its icons

---
+++ frontend/src/components/header.js
@@ -0,0 +1,119 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import SearchBar from "../components/searchbar";
+
+function Header() {return (
+    <>
+      {/* ---------- HEADER ---------- */}
+      <div className="header">
+        <a>
+          <img src="../images/reddit-logo.png" className="reddit-logo" />
+        </a>
+
+        <SearchBar />
+
+        <ul className="header-actions">
+          <li className="header-action">
+            <button>
+              <img src="../images/ads.svg" />
+              <div className="header-action-tooltip">Advertise on Reddit</div>
+            </button>
+          </li>
+
+          <li className="header-action">
+            <button>
+              <a href="./chats.html" className="header-action-link">
+                <img src="../images/chat.svg" />
+                <div className="header-action-tooltip">Open chat</div>
+              </a>
+            </button>
+          </li>
+
+          <li className="header-action">
+            <Link to="/create_post" className="header-action-link">
+              <img src="../images/create-post.svg" /> Create
+              <div className="header-action-tooltip">Create post</div>
+            </Link>
+          </li>
+
+          <li className="header-action">
+            <button className="inbox-button">
+              <img src="../images/open-inbox.svg" />
+              <div className="notification-counter">1</div>
+              <div className="header-action-tooltip">Open inbox</div>
+            </button>
+          </li>
+
+          <li className="header-action">
+            <button className="profile-menu-button">
+              <label htmlFor="profile-menu-visibility-checkbox">
+                <img src="../images/avatar.png" className="header-action-avatar" />
+              </label>
+              <div className="online-indicator"></div>
+              <div className="header-action-tooltip">Open profile menu</div>
+            </button>
+            <input type="checkbox" className="profile-menu-visibility" id="profile-menu-visibility-checkbox" />
+            <ul className="profile-menu">
+
+              <li className="profile-menu-item">
+              <Link to="/viewprofile" className="profile-menu-link">
+                <div className="profile-menu-item-icon">
+                  <img
+                    src="../images/avatar.png"
+                    className="profile-menu-item-icon-avatar"
+                    alt="Profile avatar"
+                  />
+                  <div className="online-indicator online-indicator-profile-menu"></div>
+                </div>
+                <div className="profile-menu-item-info">
+                  <div className="profile-menu-item-title">View Profile</div>
+                </div>
+              </Link>
+            </li>
+
+            <li className="profile-menu-item">
+              <Link to="/edit-avatar" className="profile-menu-link">
+                <div className="profile-menu-item-icon">
+                  <img src="../images/edit-avatar.svg" alt="Edit avatar icon" />
+                </div>
+                <div className="profile-menu-item-info">
+                  <div className="profile-menu-item-title">Edit Avatar</div>
+                </div>
+              </Link>
+            </li>
+
+              <li className="profile-menu-item">
+                <div className="profile-menu-link">
+                  <div className="profile-menu-item-icon">
+                    <img src="../images/dark-mode.svg" />
+                  </div>
+                  <div className="profile-menu-item-info">
+                    <div className="profile-menu-item-title">Dark Mode</div>
+                    <label className="toggle-switch">
+                      <input type="checkbox" />
+                      <span className="slider"></span>
+                    </label>
+                  </div>
+                </div>
+              </li>
+
+              <li className="profile-menu-item">
+                <Link to="/login" className="profile-menu-link">
+                  <div className="profile-menu-item-icon">
+                    <img src="../images/logout.svg" alt="Logout icon" />
+                  </div>
+                  <div className="profile-menu-item-info">
+                    <div className="profile-menu-item-title">Log Out</div>
+                  </div>
+                </Link>
+              </li>
+              
+            </ul>
+          </li>
+        </ul>
+      </div>
+      </>
+)};
+
+export default Header;
--- frontend/src/pages/viewprofile.js
+++ frontend/src/pages/viewprofile.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import Sidebar from "../components/sidebar";
 import SearchBar from "../components/searchbar";
+import Header from "../components/header";
 
 function ViewProfile() {
   useEffect(() => {
@@ -47,132 +48,7 @@ function ViewProfile() {
   return (
 
     <>
-       <div className="header">
-        <a>
-          <img src="../images/reddit-logo.png" className="reddit-logo" />
-        </a>
-
-        <SearchBar/>
-
-        <ul className="header-actions">
-          <li className="header-action">
-            <button>
-              <img src="../images/ads.svg" />
-              <div className="header-action-tooltip">
-                Advertise on Reddit
-              </div>
-            </button>
-          </li>
-          <li className="header-action">
-            <button>
-              <a href="./chats.html" className="header-action-link">
-                <img src="../images/chat.svg" />
-                <div className="header-action-tooltip">
-                  Open chat
-                </div>
-              </a>
-            </button>
-          </li>
-          <li className="header-action">
-            <Link to ="/create_post" className="header-action-link">
-              <img src="../images/create-post.svg" /> Create
-              <div className="header-action-tooltip">
-                Create post
-              </div>
-            </Link>
-          </li>
-          <li className="header-action">
-            <button className="inbox-button">
-              <img src="../images/open-inbox.svg" />
-              <div className="notification-counter">1</div>
-              <div className="header-action-tooltip">
-                Open inbox
-              </div>
-            </button>
-          </li>
-          <li className="header-action">
-            <button className="profile-menu-button">
-              <label for="profile-menu-visibility-checkbox">
-                <img src="../images/avatar.png" className="header-action-avatar" />
-              </label>
-              <div className="online-indicator"></div>
-              <div className="header-action-tooltip">
-                Open profile menu
-              </div>
-            </button>
-            <input type="checkbox" className="profile-menu-visibility" id="profile-menu-visibility-checkbox" />
-            <ul className="profile-menu">
-              <li className="profile-menu-item">
-                <Link to="/viewprofile" className="header-action-link">
-                  <div className="profile-menu-item-left">
-                    <img src="../images/avatar.png"
-                      className="profile-menu-item-icon profile-menu-item-icon-avatar" />
-                    <div className="online-indicator online-indicator-profile-menu"></div>
-                  </div>
-                  <div className="profile-menu-item-right">
-                    <div className="profile-menu-item-title">
-                      View Profile
-                    </div>
-                    <div className="profile-menu-item-info-extra">
-
-                    </div>
-                  </div>
-                </Link>
-              </li>
-              <li className="profile-menu-item">
-                <Link to="/edit-avatar" className="header-action-link">
-                  <div className="profile-menu-item-icon">
-                    <img src="../images/edit-avatar.svg" />
-                  </div>
-                  <div className="profile-menu-item-info">
-                    <div className="profile-menu-item-title">
-                      Edit Avatar
-                    </div>
-                  </div>
-                </Link>
-              </li>
-              <li className="profile-menu-item">
-                <div className="profile-menu-item-icon">
-                  <img src="../images/achievements.svg" />
-                </div>
-                <div className="profile-menu-item-info">
-                  <div className="profile-menu-item-title">
-                    Achievements
-                  </div>
-                  <div className="profile-menu-item-info-extra">
-                    3 unlocked
-                  </div>
-                </div>
-              </li>
-              <li className="profile-menu-item">
-                <div className="profile-menu-item-icon">
-                  <img src="../images/dark-mode.svg" />
-                </div>
-                <div className="profile-menu-item-info">
-                  <div className="profile-menu-item-title">
-                    Dark Mode
-                  </div>
-                  <label className="toggle-switch">
-                    <input type="checkbox" />
-                    <span className="slider"></span>
-                  </label>
-                </div>
-              </li>
-              <li className="profile-menu-item" style={{ cursor: "pointer" }}>
-                <Link to="/login" className="profile-menu-link">
-                  <div className="profile-menu-item-icon">
-                    <img src="../images/logout.svg" alt="Logout icon" />
-                  </div>
-                  <div className="profile-menu-item-info">
-                    <div className="profile-menu-item-title">Log Out</div>
-                  </div>
-                </Link>
-              </li>
-
-            </ul>
-          </li>
-        </ul>
-      </div>
+      <Header />
       <Sidebar/>
       <div className="main">
         <div className="profile-header">
