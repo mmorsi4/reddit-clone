2e274279ca47629e49da09aaae31d4183d0218cb
Mohamed Morsi <mo.morsi2004@gmail.com>
2025-11-01 00:00:57 +0200

    Automatically detect community when posting

--- backend/src/controllers/users.js
+++ backend/src/controllers/users.js
@@ -5,6 +5,7 @@ export async function getProfile(req,res){
   if(!user) return res.status(404).json({message:'Not found'});
   res.json(user);
 }
+
 export async function me(req,res){
   const user = await User.findById(req.userId).select('-passwordHash');
   res.json(user);
