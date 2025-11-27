3e798643d4bc03b0a2378e661f4e778f9f137762
Mohamed Morsi <mo.morsi2004@gmail.com>
2025-11-28 00:33:28 +0200

    Completed search functionality

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { authMiddleware } from '../middleware/auth.js';
-import { getProfile, me, getProfileByUsername, getRecentCommunities, updateRecentCommunities } from '../controllers/users.js';
+import { getProfile, me, getUsers, getProfileByUsername, getRecentCommunities, updateRecentCommunities } from '../controllers/users.js';
 const router = express.Router();
 
 router.get('/recent-communities', authMiddleware, getRecentCommunities);
 router.get('/me', authMiddleware, me);
+router.get('/', getUsers); // must be the last placed route
 router.get('/id/:id', getProfile);
 router.get('/:username', getProfileByUsername); // must be the last placed route
 router.post('/recent-communities', authMiddleware, updateRecentCommunities);
