1bea5531a857c325715cd15b0f3476cfd3dbce1b
Zeyad <zdarkzy2005@gmail.com>
2025-11-29 12:16:21 +0200

    Changed login and signup UI

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -2,12 +2,49 @@ import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "../styles/auth.css";
 
-// test nothing vdsicusicwdijc
+
 function Login() {
   const [activeForm, setActiveForm] = useState("login");
   const [showPassword, setShowPassword] = useState(false);
   const [users, setUsers] = useState([]);
   const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [pass, setPass] = useState("");
+  const [email, setEmail] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [PassFocused, setPassFocused] = useState(false);
+  const [NameFocused, setNameFocused] = useState(false);
+  const [EmailFocused, setEmailFocused] = useState(false);
+  const [valid, setValid] = useState(true);
+
+
+  const WarningIcon = () => (
+  <svg 
+    width="20" 
+    height="20" 
+    viewBox="0 0 24 24" 
+    fill="none" 
+    className="icon-warn"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="12" cy="12" r="10" stroke="#ff4d4d" stroke-width="2" />
+    <path d="M12 8V12" stroke="#ff4d4d" stroke-width="2" stroke-linecap="round" />
+    <circle cx="12" cy="16" r="1.5" fill="#ff4d4d" />
+  </svg>
+);
+
+const SuccessIcon = () => (
+  <svg 
+    width="20" 
+    height="20" 
+    viewBox="0 0 24 24" 
+    fill="none" 
+    className="icon-success" // We will style this below
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M5 13L9 17L19 7" stroke="#28a745" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/> 
+  </svg>
+);
 
   // Load existing users from localStorage
   useEffect(() => {
@@ -23,10 +60,10 @@ function Login() {
     e.preventDefault();
     const usernameOrEmail = e.target.elements["login-username-email"].value.trim();
     const password = e.target.elements["login-password"].value.trim();
-
+    setIsSubmitted(true);
     // Validation
     if (usernameOrEmail === "" || password === "") {
-      alert("Please fill out all fields.");
+
       return;
     }
 
@@ -39,10 +76,11 @@ function Login() {
 
       const data = await res.json();
       if (res.ok) {
-        alert(`Welcome back, ${data.user.username}!`);
+        //HANDLE SUCCESSFUL LOGIN
         navigate("/home");
-        //setTimeout(() => setActiveForm("login"), 1500);
-      } else {
+      } 
+      else {
+        setValid(false);
         alert(res.status || "Invalid credentials.");
         alert(data.message || "Invalid credentials.");
       }
@@ -50,6 +88,7 @@ function Login() {
       console.error(err);
       alert("Error connecting to server.");
     }
+  };
 
     // // Find matching user
     // const user = users.find(
@@ -64,9 +103,10 @@ function Login() {
     // } else {
     //   alert("Invalid username/email or password. Try again!");
     // }
-  };
+
 
   //  Handle Signup
+
   const handleSignup = async (e) => {
     e.preventDefault();
     const username = e.target.elements["signup-username"].value.trim();
@@ -75,7 +115,7 @@ function Login() {
 
     // Validation
     if (username === "" || email === "" || password === "") {
-      alert("Please fill out all fields.");
+      setIsSubmitted(true);
       return;
     }
 
@@ -113,7 +153,7 @@ function Login() {
   return (
     <div>
       {/* HEADER */}
-      <div className="header">
+      <div className="head">
         <Link to="/">
           <img src="../images/reddit-logo.png" className="reddit-logo" alt="Reddit Logo" />
         </Link>
@@ -121,9 +161,10 @@ function Login() {
 
       {/* AUTH BOX */}
       <div className="login-container">
-        <div className="login-box">
-          <div className="login-tabs">
-            <button
+        <div className={activeForm === "login" ? "login-box" : "signup-box"}>.
+
+          {/*<div className="login-tabs">
+             <button
               className={`login-tab ${activeForm === "login" ? "active" : ""}`}
               onClick={() => setActiveForm("login")}
             >
@@ -134,68 +175,111 @@ function Login() {
               onClick={() => setActiveForm("signup")}
             >
               Sign Up
-            </button>
-          </div>
+            </button> 
+          </div>*/}
+
 
           {/* 🟢 Login Form */}
+
           {activeForm === "login" && (
             <form className="login-form login-active" id="login-form" onSubmit={handleLogin}>
-              <h2>Welcome Back!</h2>
+              <h2>Log In</h2>
+              <p>By continuing, you agree to our <a href="https://redditinc.com/policies/user-agreement">User Agreement</a> and
+                acknowledge that you understand the <a href="https://www.reddit.com/policies/privacy-policy">Privacy Policy</a>.</p>
+
+                <div className="inputbox">
               <input
                 id="login-username-email"
                 type="text"
-                placeholder="Username or Email"
-                required
-              />
-              <div className="login-password-field">
+                className={ isSubmitted && !name && !NameFocused ? "warning" : ""}
+                onFocus={() => setNameFocused(true)}
+                onBlur={() => setNameFocused(false)}
+                placeholder="Email or username"
+                onChange={(e) => setName(e.target.value)}
+               />
+                {isSubmitted && !name && !NameFocused && <WarningIcon />}
+                {name && !NameFocused && <SuccessIcon />}
+              <p className={`warn ${ isSubmitted && !name && !NameFocused ? "show" : ""}`}>Please fill out this field.</p>
+              </div>
+              
+              <div className="inputbox">
                 <input
                   id="login-password"
                   type={showPassword ? "text" : "password"}
+                  className={ isSubmitted && !pass && !PassFocused ? "warning" : ""}
+                  onFocus={() => setPassFocused(true)}
+                  onBlur={() => setPassFocused(false)}
                   placeholder="Password"
-                  required
+                  onChange={(e) => {setPass(e.target.value); setValid(true)}}
                 />
-                <img
-                  src={showPassword ? "../images/eye-off.svg" : "../images/eye.svg"}
-                  alt="Toggle password visibility"
-                  className="login-toggle-password"
-                  onClick={togglePassword}
-                  style={{ cursor: "pointer" }}
-                />
-              </div>
-              <button type="submit" className="login-btn">Login</button>
+                {isSubmitted && !pass && !PassFocused && <WarningIcon />}
+                {pass && !PassFocused && <SuccessIcon />}
+                <p className={`warn ${ isSubmitted && !pass && !PassFocused ? "show" : ""}`}>{isSubmitted && pass && !valid ? "Invalid username or password." : "Please fill out this field."}</p>
+                </div>
+
               <p className="login-switch-text">
-                Don’t have an account?{" "}
-                <span onClick={() => setActiveForm("signup")}>Sign up</span>
+              <span>Forgot Password?</span>
               </p>
+              <p className="login-switch-text">
+                New to Reddit?{" "}
+                <span onClick={() => {setActiveForm("signup"); setIsSubmitted(false) }}>Sign up</span>
+              </p>
+              <button type="submit" className={(!pass || !name) ? "login-button" :"login-btn"}>Log In</button>
             </form>
           )}
 
           {/* 🟣 Signup Form */}
           {activeForm === "signup" && (
             <form className="login-form login-active" id="signup-form" onSubmit={handleSignup}>
-              <h2>Create an Account</h2>
-              <input id="signup-username" type="text" placeholder="Username" required />
-              <input id="signup-email" type="email" placeholder="Email" required />
-              <div className="login-password-field">
-                <input
-                  id="signup-password"
-                  type={showPassword ? "text" : "password"}
-                  placeholder="Password (min 6 chars)"
-                  required
-                />
-                <img
-                  src={showPassword ? "../images/eye-off.svg" : "../images/eye.svg"}
-                  alt="Toggle password visibility"
-                  className="login-toggle-password"
-                  onClick={togglePassword}
-                  style={{ cursor: "pointer" }}
-                />
+              <h2>Sign Up</h2>
+              <p>By continuing, you agree to our <a href="https://redditinc.com/policies/user-agreement">User Agreement</a> and
+                acknowledge that you understand the <a href="https://www.reddit.com/policies/privacy-policy">Privacy Policy</a>.</p>
+              <div className="inputbox">
+              <input 
+              id="signup-username"
+              type="text"
+              className={ isSubmitted && !name && !NameFocused ? "warning" : ""}
+              onFocus={() => setNameFocused(true)}
+              onBlur={() => setNameFocused(false)}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Username"
+              />
+              {isSubmitted && !name && !NameFocused && <WarningIcon />}
+              <p className={`warn ${ isSubmitted && !name && !NameFocused ? "show" : ""}`}>Please fill out this field.</p>
               </div>
-              <button type="submit" className="login-btn">Sign Up</button>
+              
+              <div className="inputbox">
+              <input
+              id="signup-email"
+              type="email"
+              className={ isSubmitted && !email && !EmailFocused ? "warning" : ""}
+              onFocus={() => setEmailFocused(true)}
+              onBlur={() => setEmailFocused(false)}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+              />
+              {isSubmitted && !email && !EmailFocused && <WarningIcon />}
+              <p className={`warn ${ isSubmitted && !email && !EmailFocused ? "show" : ""}`}>Please fill out this field.</p>
+              </div>
+
+              <div className="inputbox">
+              <input
+              id="signup-password"
+              className={ isSubmitted && !pass && !PassFocused ? "warning" : ""}
+              onFocus={() => setPassFocused(true)}
+              onBlur={() => setPassFocused(false)}
+              onChange={(e) => setPass(e.target.value)}
+              placeholder="Password (min 6 chars)"
+              />
+              {isSubmitted && !pass && !PassFocused && <WarningIcon />}
+              <p className={`warn ${ isSubmitted && !pass && !PassFocused ? "show" : ""}`}>Please fill out this field.</p>
+              </div>
+
               <p className="login-switch-text">
-                Already have an account?{" "}
-                <span onClick={() => setActiveForm("login")}>Login</span>
+                Already a redditor?{" "}
+                <span onClick={() => {setActiveForm("login"); setIsSubmitted(false) ; setName("") ; setPass("")}}>Log In</span>
               </p>
+              <button type="submit" className={(!pass || !name || !email) ? "login-button" :"login-btn"}>Sign Up</button>
             </form>
           )}
         </div>
